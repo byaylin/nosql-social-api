67dddc158797ff8283743cc119648d8ecc8f5bec
byaylin <aylinxmorales@gmail.com>
2023-03-14 22:25:07 -0700

    updates

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const allRoutes = require('./routes');
-const db = require('../nosql-social-api/config/connection');
+const db = require('./config/connection');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.json());
 app.use(allRoutes);
 
 db.once('open', () => {
